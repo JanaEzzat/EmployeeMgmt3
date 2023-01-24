9e15d34a7a0a920571d77bff155667a0d84c9f1e
JanaEzzat <janaezzat657@gmail.com>
2023-01-24 23:46:37 +0200

    change position

--- EmployeeMgmt3/login.Designer.cs
+++ EmployeeMgmt3/login.Designer.cs
@@ -39,6 +39,7 @@
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
 
