a737327296fb3ef1cf714145f5b31b23279095bf
JanaEzzat <janaezzat657@gmail.com>
2023-01-25 13:17:14 +0200

    add font 12

--- EmployeeMgmt3/login.Designer.cs
+++ EmployeeMgmt3/login.Designer.cs
@@ -101,9 +101,10 @@
             // label3
             // 
             this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(253, 115);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(36, 16);
+            this.label3.Size = new System.Drawing.Size(50, 22);
             this.label3.TabIndex = 5;
             this.label3.Text = "label3";
             // 
