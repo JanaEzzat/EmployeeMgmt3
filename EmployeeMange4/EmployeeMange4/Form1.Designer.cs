8b398cffb3a8444ff06de1d40f305b19102061c7
JanaEzzat <janaezzat657@gmail.com>
2023-01-25 18:28:57 +0200

    add lable

--- EmployeeMange4/EmployeeMange4/Form1.Designer.cs
+++ EmployeeMange4/EmployeeMange4/Form1.Designer.cs
@@ -36,6 +36,7 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -87,7 +88,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(280, 294);
+            this.textBox2.Location = new System.Drawing.Point(280, 249);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(184, 27);
             this.textBox2.TabIndex = 5;
@@ -95,11 +96,19 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(276, 267);
+            this.label3.Location = new System.Drawing.Point(276, 222);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(56, 24);
+            this.label3.Size = new System.Drawing.Size(0, 24);
             this.label3.TabIndex = 4;
-            this.label3.Text = "name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(282, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 24);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "password";
             // 
             // Form1
             // 
@@ -107,6 +116,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(1386, 788);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox1);
@@ -135,6 +145,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
