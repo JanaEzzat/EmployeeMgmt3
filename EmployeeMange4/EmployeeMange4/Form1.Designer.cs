98b9f66e20573eedfa825c906072865d95cc00a5
JanaEzzat <janaezzat657@gmail.com>
2023-01-25 18:21:40 +0200

    add text

--- EmployeeMange4/EmployeeMange4/Form1.Designer.cs
+++ EmployeeMange4/EmployeeMange4/Form1.Designer.cs
@@ -45,11 +45,11 @@
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.Color.Teal;
-            this.label1.Location = new System.Drawing.Point(508, 34);
+            this.label1.Location = new System.Drawing.Point(472, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 22);
+            this.label1.Size = new System.Drawing.Size(125, 22);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Employee M S";
             // 
             // Form1
             // 
