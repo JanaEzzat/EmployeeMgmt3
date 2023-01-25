533b041162f9b33013f9a6f4e62f4c7801d91378
JanaEzzat <janaezzat657@gmail.com>
2023-01-25 18:43:33 +0200

    add bakground teal

--- EmployeeMange4/EmployeeMange4/Employee.Designer.cs
+++ EmployeeMange4/EmployeeMange4/Employee.Designer.cs
@@ -33,6 +33,8 @@
             // 
             // panel1
             // 
+            this.panel1.BackColor = System.Drawing.Color.Teal;
+            this.panel1.ForeColor = System.Drawing.Color.Teal;
             this.panel1.Location = new System.Drawing.Point(351, 98);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(200, 100);
