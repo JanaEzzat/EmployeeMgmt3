c42316941209bd0ca1954b9ce9846c79dcee0bd7
JanaEzzat <janaezzat657@gmail.com>
2023-01-25 18:18:36 +0200

    add dock left

--- EmployeeMange4/EmployeeMange4/Form1.Designer.cs
+++ EmployeeMange4/EmployeeMange4/Form1.Designer.cs
@@ -34,9 +34,10 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Teal;
-            this.panel1.Location = new System.Drawing.Point(405, 149);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(200, 100);
+            this.panel1.Size = new System.Drawing.Size(200, 749);
             this.panel1.TabIndex = 0;
             // 
             // Form1
