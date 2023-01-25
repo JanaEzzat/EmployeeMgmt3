f4acc4a46b6ff88aebd9df24d3c94e5368a3f6e0
JanaEzzat <janaezzat657@gmail.com>
2023-01-25 21:10:48 +0200

    add size

--- EmployeeMange4/EmployeeMange4/salaries.Designer.cs
+++ EmployeeMange4/EmployeeMange4/salaries.Designer.cs
@@ -35,8 +35,9 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1200, 692);
+            this.ClientSize = new System.Drawing.Size(1200, 657);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "salaries";
             this.Text = "salaries";
