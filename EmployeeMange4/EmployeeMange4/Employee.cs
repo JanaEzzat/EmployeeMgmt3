c49f131678a4520da14b1b049f0cac5e2c37a6f1
JanaEzzat <janaezzat657@gmail.com>
2023-01-27 17:23:42 +0200

    add code

--- EmployeeMange4/EmployeeMange4/Employee.cs
+++ EmployeeMange4/EmployeeMange4/Employee.cs
@@ -48,3 +48,9 @@ DepCb.SelectedIndex = -1;
    int key = 0;
 private void EmployeeList_CellContentClick(object sender, DataGridViewCellEventArgs e)
 {
+    EmpNameTb.Text = EmployeeList.SelectedRows[0].Cells[1].Value.ToString();
+    GenCb.Text = EmployeeList.SelectedRows[0].Cells[2].Value.ToString();
+    DepCb.SelectedValue = EmployeeList.SelectedRows[0].Cells[3].Value.ToString();
+    DOBTb.Text = EmployeeList.SelectedRows[0].Cells[4].Value.ToString();
+    JDateTb.Text = EmployeeList.SelectedRows[0].Cells[5].Value.ToString();
+    DailySalTb.Text = EmployeeList.SelectedRows[0].Cells[6].Value.ToStrin
