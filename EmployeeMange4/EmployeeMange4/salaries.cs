57c5d549717ea07fd99e7f3aae74c0a6351f8f6b
JanaEzzat <janaezzat657@gmail.com>
2023-01-27 17:47:39 +0200

    add sntyex

--- EmployeeMange4/EmployeeMange4/salaries.cs
+++ EmployeeMange4/EmployeeMange4/salaries.cs
@@ -80,3 +80,11 @@ namespace EmployeeMgmt1
                 int Amount = DSal * Convert.ToInt32(DaysTb.Text);
                 int Days = Convert.ToInt32(DaysTb.Text);
                 string Query = "Update SalaryTb1 values({0},{1},'{2}',{3},'{4}',)";
+                { 3},'{4}',)";
+                    Query = string.Format(Query, EmpCb.SelectedValue.ToString(), Days, Period, Amount, DateTime.Today.Date);
+                Con.SetData(Query);
+                ShowSalary();
+                { 3},'{4}',)";
+                    Query = string.Format(Query, EmpCb.SelectedValue.ToString(), Days, Period, Amount, DateTime.Today.Date);
+                Con.SetData(Query);
+                ShowSalary();
